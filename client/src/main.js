b4e3fafd70b2a5e5b9b5b6d4f1ec3406e35d91e4
sogood99 <dingjh19@mails.tsinghua.edu.cn>
2021-07-20 12:11:40 +0800

    Changed echarts version and deleted some comments

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import echarts from "echarts";
-// Vue.prototype.$echarts = echarts;
 
 createApp(App)
   .use(router)
